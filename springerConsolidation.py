09d330d4f4bd2c666830c07066551d2d0def13c0
dhiraj-v <64371471+dhiraj-v@users.noreply.github.com>
2020-12-14 21:08:31 +0530

    updated springerConsolidation.py
    The function takes the url string as the input and it's modified to return the list of [pages, keywords, citation count, University and the abstract]
    In the end we can find the sample that is used as an input.

--- springerConsolidation.py
+++ springerConsolidation.py
@@ -8,6 +8,10 @@ def extractDetailSpringer(url):
     pages = ''
     citations = ''
     uniList = ''
+    returnData = []
+    '''
+    pages, keywords, citeCount, university, abstract = 
+    '''
     req = requests.get(url)
     soup = BeautifulSoup(req.text, "html.parser")
     prettyHTML = soup.prettify()
@@ -20,25 +24,38 @@ def extractDetailSpringer(url):
     #print('PAGES: ')
     regexPages = r'(?!0|1$)\d{1,4}[-](?!0|1$)\d{1,4}'
     pages = re.search("(?!0|1$)\d{1,4}[-](?!0|1$)\d{1,4}", str(pageNumbers.contents[0])).group()
-    #print(pages)
+    #print(type(pages))
+    returnData.append(pages)
     #print('KEYWORDS: ')
     for word in keywords: 
         authorKeywords += str(word.contents)[2:-6]
         authorKeywords += '; '
     #print(authorKeywords)
+    returnData.append(authorKeywords)
     #print('CITATIONS: ')
     if not citationCount:
         citations = '0'
     else: 
         citations = (str(citationCount.contents)[2:-2])
     #print(citations)
+    returnData.append(citations)
     #print("UNIVERSITIES: ")
     for uni in universities: 
         uniList += str(uni.contents)[2:-2]
         uniList += '; '
     #print(uniList)
+    returnData.append(uniList)
     #print('ABSTRACT: ')
     for para in abstractParas: 
         abstract += str(para.contents)[2:-2]
     #print(abstract)
-    return pages, authorKeywords, citations, uniList, abstract
+    returnData.append(abstract)
+    return returnData
+'''
+#SAMPLE
+
+#url = 'https://link.springer.com/chapter/10.1007/978-3-319-94496-8_10'
+url = "https://link.springer.com/chapter/10.1007/978-3-030-30275-7_20"
+urlDetails = extractDetailSpringer(url)
+print(urlDetails)
+'''
